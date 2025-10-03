319d7393c25684bb7e17ab953a5a277f07743c17
iesramonllullluca <al.lucamartini@iesramonllull.net>
2025-10-03 09:39:27 +0200

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,6 @@
+function restar() {
+    let valor1 = parseInt(document.getElementById('numero1').value);
+    let valor2 = parseInt(document.getElementById('numero2').value);
+    let resultado = valor1 - valor2;
+    alert("El resultat és " + resultado);
+  }
